15ff86ba7e19383b686f9faf6f8090abe8d3b403
Jonas York <jyork26@cpatech.org>
2024-12-11 15:32:24 +0000

    my notes

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-# # Implementation & testing of the ArrayStack class
+# Jonas York 
+# U4 L3 
+# Reversing  a file 
 
 from StackClass import ArrayStack
 
+def getFile(fileName):
+  cleanContents = ""
+  with open(fileName, "r") as openFile:
+      contents = openFile.read()
+      for letter in contents:
+        if ord(letter) >= 65 and ord(letter) <= 90 or ord(letter) >= 97 and ord(letter) <= 122:
+          cleanContents += letter
+        elif letter == " " or "\n":
+          cleanContents += letter
+        
+  return cleanContents
+
+def writeFile(fileName, contents):
+  with open(fileName, "w") as openFile:
+    openFile.write(contents)
+
 
 def main():
-    original = "Sphinx of black quartz, judge my vow"
-    new = ""
+  myStack = ArrayStack()
+  reverse = ""
+  contents = getFile("earnest.txt")
+  contentsList = contents.split(" ")
 
-    MyStack = ArrayStack()
-
-    for letter in original:
-        MyStack.push(letter)
-    
-    for item in range(len(MyStack)):
-        new += MyStack.pop()
-
-    print(f"Original: {original}")
-    print(f"Reversed: {new}")
+  for item in contentsList:
+    myStack.push(item)
+  for i in range(len(myStack)):
+    reverse += myStack.pop() + " "
+  writeFile("reverse.txt", reverse)
+  
 
 if __name__ == "__main__":
     main()
